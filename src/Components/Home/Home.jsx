a1de07d86ae88bb6c66f28d93d5c8e60e1a78921
Abhilash Reddy Kothuru <kothuruabhilashreddy@gmail.com>
2024-06-07 03:14:24 -0400

    Fetches input file attributes list

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import FileUpload from '../FileUpload/FileUpload';
+import BarChart from '../BarChart/BarChart';
 
 const Home = () => {
-    
+    const [plotData, setPlotData] = useState(null);
+    const [attributes, setAttributes] = useState([]);
+    console.log('attributes:', attributes);
     return (
         <div className="Home">
-            <FileUpload />
+            { <FileUpload setPlotData={setPlotData} setAttributes={setAttributes} />}
+            {/* {plotData && <BarChart plotData={plotData} xLabel="wedw" yLabel="wcwe"/>} */}
         </div>
     )
 }
